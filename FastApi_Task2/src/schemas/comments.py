dfcc06fb469176d6703a689053fa5dcddde3db88
Akhmetov Igor <mcventik@yandex.ru>
2026-03-10 13:12:40 +0400

    remove old src directory, added new main

--- FastApi_Task2/src/schemas/comments.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, Field
-from datetime import datetime
-
-
-class Comment(BaseModel):
-    text: str
-    post_id: int
-    author_id: int
-    created_at: datetime = Field(default_factory=datetime.now, frozen=True)
--- FastApi_Task2/src/schemas/locations.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field
-
-
-class Location(BaseModel):
-    name: str = Field(max_length=256)
-    is_published: bool = True
